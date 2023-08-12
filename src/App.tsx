b0dddc083df7bffe756b604c258c4894c6a24bae
김하연 <hayeon@finda.co.kr>
2023-08-12 14:36:16 +0900

    feat: react-router-dom 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<h1>Home Page</h1>} />
+      <Route path="/posts" element={<h1>Post List Page</h1>} />
+      <Route path="/posts/:id" element={<h1>Post Detail Page</h1>} />
+      <Route path="/posts/new" element={<h1>Post New Page</h1>} />
+      <Route path="/posts/edit/:id" element={<h1>Post Edit Page</h1>} />
+      <Route path="/profile" element={<h1>Profile Page</h1>} />
+      <Route path="/profile/edit" element={<h1>Profile Edit Page</h1>} />
+      <Route path="/notifications" element={<h1>Notifications Page</h1>} />
+      <Route path="/search" element={<h1>Search Page</h1>} />
+      <Route path="/users/login" element={<h1>Login Page</h1>} />
+      <Route path="/users/signup" element={<h1>Signup Page</h1>} />
+      <Route path="*" element={<Navigate replace to="/" />} />
+    </Routes>
   );
 }
 
