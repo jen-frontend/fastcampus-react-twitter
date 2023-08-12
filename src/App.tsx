ca9ee71e0a5de62573d6d32ce5c3d7b0fad9572a
jen-frontend <jen.frontend@gmail.com>
2023-08-12 19:32:44 +0900

    feat: Firebase Auth 세팅하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 import Router from "components/Router";
 import { Layout } from "components/Layout";
 
+import { getAuth } from "firebase/auth";
+import { app } from "firebaseApp";
+
 function App() {
+  const auth = getAuth(app);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+  );
+
+  console.log(auth, isAuthenticated);
+
   return (
     <Layout>
       <Router />
