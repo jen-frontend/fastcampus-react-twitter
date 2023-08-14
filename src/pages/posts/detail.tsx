ce0d84ac83cae7e0db6519b7f291c97a539c4515
jen-frontend <jen.frontend@gmail.com>
2023-08-14 12:35:47 +0900

    feat: 댓글 기능 구현

--- src/pages/posts/detail.tsx
+++ src/pages/posts/detail.tsx
@@ -1,13 +1,15 @@
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
 
 import Loader from "components/loader/Loader";
 import PostBox from "components/posts/PostBox";
 import { PostProps } from "pages/home";
 import { useCallback, useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { db } from "firebaseApp";
 
 import PostHeader from "components/posts/PostHeader";
+import CommentForm from "components/comments/CommentForm";
+import CommentBox, { CommentProps } from "components/comments/CommentBox";
 
 export default function PostDetail() {
   const params = useParams();
@@ -16,9 +18,9 @@ export default function PostDetail() {
   const getPost = useCallback(async () => {
     if (params.id) {
       const docRef = doc(db, "posts", params.id);
-      const docSnap = await getDoc(docRef);
-
-      setPost({ ...(docSnap?.data() as PostProps), id: docSnap?.id });
+      onSnapshot(docRef, (doc) => {
+        setPost({ ...(doc?.data() as PostProps), id: doc.id });
+      });
     }
   }, [params.id]);
 
@@ -29,7 +31,20 @@ export default function PostDetail() {
   return (
     <div className="post">
       <PostHeader />
-      {post ? <PostBox post={post} /> : <Loader />}
+      {post ? (
+        <>
+          <PostBox post={post} />
+          <CommentForm post={post} />
+          {post?.comments
+            ?.slice(0)
+            ?.reverse()
+            ?.map((data: CommentProps, index: number) => (
+              <CommentBox data={data} key={index} post={post} />
+            ))}
+        </>
+      ) : (
+        <Loader />
+      )}
     </div>
   );
 }
